a7182ba4ad87bda27ca772294c22300c7f613f9d
shraddha dabade <118508539+shraddha-30@users.noreply.github.com>
2022-12-07 20:05:55 +0530

    Create MainActivity2.java

---
+++ MainActivity2.java
@@ -0,0 +1,11 @@
+package com.example.intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+public class MainActivity2 extends AppCompatActivity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main2);
+}
+}
